355346b4afc847d33107b9423f7108ebe4034fb2
Durga Prasad <119026227+durgaaprasadch@users.noreply.github.com>
2025-01-16 11:17:17 +0530

    Add files via upload

---
+++ Arithmetic Operators....cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a = 5;
+	int b = 4;
+	
+	//Arithmetic  Operators -> + - * / %
+	
+	// a + b;
+	// a - b;
+	// a * b;
+	
+	// cout<< a++; 5
+	
+	// cout <<++a; 7
+	
+	//cout <<a++ + ++a; = 12
+	
+	//Assignment Operators -> = +=  -= /= %=
+	
+//	 a += b ;
+//	
+//	cout <<a;
+
+     //Relational Operators ->  == != > >= <=
+     
+//     
+//     bool ans = a < b;
+//     cout << ans;
+
+
+    //Logical Operators ->  && || !
+    
+    // && -> must all ;
+    // || ->  anyone;
+    
+    // bool ans = (a > 2) || (b < 4);
+    
+    // cout<<ans;
+      
+      
+    /*  
+	
+	
+	 cond1    cond2    &&     ||
+      
+      T         T      T      T
+      T         F      F      T
+      F         T      F      T
+      F         F      F      F
+    
+	
+	
+	*/
+	
+	
+	
+//	if else 
+  
+//      Ternary  
+
+  //    (condition) ? statement 1 : statement 2
+//	int num;
+//	cin>>num;
+//	
+//	string ans = num > 0 ? "Positive" : "Negative";
+//	cout<<ans;
+	
+	//nested ternary
+	
+	//   string ans = num == 0 ?"Zero" : ((num > 0)?"Positive": "Negative")
+	
+	
+	
+	
+	//---------------------------------------------------------------------------------
+	
+	
+	
+	//Loops
+	
+	//For loop
+	
+	//  for(init ; termination cond ; update){
+    //	 }
+	
+//	for(int i=0;i<5;i++){
+//		cout<< i << " ";
+//	}
+	
+	// sum of n natural numbers
+//	int n;
+//	int sum = 0;
+//	
+//	cout <<"enter n:";
+//	cin >> n;
+//	for(int i = 1;i <= n; i++ ){
+//		sum += i;
+//	}
+//	cout << sum;
+//	
+//	
+
+
+
+
+
+//---------------------------------
+
+      //while loop
+
+//	int i=1;
+//	while(i<=5){
+//		cout<<i;
+//		i++;
+//	}
+	
+	
+	//do while
+	
+	
+	int i= 1;
+	 
+	 do{
+	 	cout<< i;
+	 	++i;
+	 }
+	 while(i <= 5);
+	
+	
+	
+     
+
+
+	 
+}
